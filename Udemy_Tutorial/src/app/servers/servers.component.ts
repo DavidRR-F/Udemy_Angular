30cff7ca51084eb0600ddbe9a6d8f7724aec658e
Rose-Franklin <david.rose-franklin@cgi.com>
2022-02-15 15:09:50 -0500

    assignment 2

--- Udemy_Tutorial/src/app/servers/servers.component.ts
+++ Udemy_Tutorial/src/app/servers/servers.component.ts
@@ -9,16 +9,35 @@ import { Component, OnInit } from '@angular/core';
   //select by atribute Ex: selector: '[app-servers]' in html <div app-servers></div>
   //select by class Ex: selector: '.app-servers' in html <div class="app-servers"></div>
   selector: 'app-servers',
-  template: `
-  <app-server></app-server>
-  <app-server></app-server>`,
+  //template: `
+  //<app-server></app-server>
+  //<app-server></app-server>`,
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
 
-  constructor() { }
+  allowNewServer = false;
+  serverCreationStatus = 'No Server Was Created';
+  serverName = '';
+
+  constructor() { 
+    // on construction after 2 seconds set allowNewServer to true
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
 
   ngOnInit(): void {
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus = 'Server ' + this.serverName + ' Was Created';
+  }
+
+  //sets server name to user input
+  onUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
   }
 
 }
