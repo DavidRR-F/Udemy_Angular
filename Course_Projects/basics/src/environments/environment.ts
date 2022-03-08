f05580e664d5b8f39ead5cf7893b5d799a401f07
DavidRR-F <99210748+DavidRR-F@users.noreply.github.com>
2022-03-08 14:47:40 -0500

    Update environment.ts

--- Course_Projects/basics/src/environments/environment.ts
+++ Course_Projects/basics/src/environments/environment.ts
@@ -7,7 +7,7 @@ export const environment = {
   signupUrl: 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=',
   loginUrl: 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=',
   fetchUrl: 'https://udemy-practice-api-default-rtdb.firebaseio.com/recipes.json',
-  firebaseAPIKey: 'AIzaSyBuVNgSelv28-Ddc3iP92_nI6UkEtxGV00'
+  firebaseAPIKey: ''
 };
 
 /*
